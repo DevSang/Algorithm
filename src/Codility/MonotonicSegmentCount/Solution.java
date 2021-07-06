654e57a78eb7bbcb706308927d8185892138eed7
SHKim-airi <dev.sanghyuk@gmail.com>
2021-07-06 14:59:48 +0900

    Codility - MonotonicSegmentCount(완료)

---
+++ src/Codility/MonotonicSegmentCount/Solution.java
@@ -0,0 +1,49 @@
+package Codility.MonotonicSegmentCount;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    public int solution(int[] A) {
+
+        List<Integer> segementArray = new ArrayList<>(A.length);
+        int diff = 0;
+        int count = 1;
+
+        for (int i = 1; i < A.length; i++) {
+            if (A[i - 1] > 1000000) {
+                return -1;
+            }
+            int newDiff = A[i] - A[i - 1];
+            if (newDiff * diff < 0
+                || (newDiff * diff == 0 && i != 1 && (newDiff != 0 || diff != 0))) {
+                segementArray.add(count);
+                count = 1;
+            }
+            if (i == A.length - 1) {
+                segementArray.add(count + 1);
+            }
+            count++;
+            diff = newDiff;
+        }
+
+        int ans = 0;
+        for (int segement : segementArray) {
+            ans += sum(segement);
+        }
+
+        if (ans > 1000000) {
+            return -1;
+        }
+
+        return ans;
+    }
+
+    public int sum(int num) {
+        if (num == 1) {
+            return 1;
+        }
+        return num * (num - 1) / 2;
+    }
+}
---
+++ src/Codility/MonotonicSegmentCount/SolutionTest.java
@@ -0,0 +1,31 @@
+package Codility.MonotonicSegmentCount;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class SolutionTest {
+
+    Solution solution = new Solution();
+
+    static List<int[]> provider() {
+        List<int[]> list = new ArrayList<>();
+//    list.add(new int[]{});
+//    list.add(new int[]{1, 2});
+        list.add(new int[]{1, 4, 2, 3, 3, 5, 6, 7, 7, 9});
+        list.add(new int[]{1, 2, 4, 7, 6, 6, 6, 2, 0});
+        return list;
+    }
+
+    @ParameterizedTest
+    @MethodSource("provider")
+    void test(int[] value) {
+
+        int solution = this.solution.solution(value);
+        System.out.println("result " + solution);
+        System.out.println("****");
+
+
+    }
+}
